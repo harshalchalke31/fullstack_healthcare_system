89ea9a98b7bded050e8c5c7b8a03771cda9667cd
harshalchalke31 <harshal.chalke2001@gmail.com>
2026-01-30 14:29:46 -0500

    Skeleton code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
-import tailwindcss from '@tailwindcss/vite'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import MyAppointment from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Appointment from './pages/Appointment'
+
 
 const App = () => {
   return (
-    <div className='text-green-500'>
-      Testing Tailwind
-      
+    <div className='mx-4, sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Home />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+        <Route path='/my-appointments' element={<MyAppointment />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,19 @@
+import doc1 from './doc1.png'
+import doc2 from './doc2.png'
+import doc3 from './doc3.png'
+import doc4 from './doc4.png'
+import doc5 from './doc5.png'
+import doc6 from './doc6.png'
+import doc7 from './do7.png'
+import doc8 from './doc8.png'
+import doc9 from './doc9.png'
+import doc10 from './doc10.png'
+import logo from './logo.png'
+import poster from './poster.png'
+
+export const assets ={
+    logo, 
+    poster,
+    
+
+}
